97b9567606782b9bd243e36df8d2dd226d92e604
Franco <franco.mana@hotmail.com>
2022-01-17 19:33:11 -0300

    Slider Level

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,10 +8,33 @@ public class UIManager : MonoBehaviour
     public Text currentScoreText;
     public Text bestScoreText;
 
+    public Slider slider;
+    public Text actualLevel;
+    public Text nextLevel;
+    public Transform topTrensform;
+    public Transform goalTrensform;
+    public Transform ball;
+
     private void Update()
     {
         currentScoreText.text = "Score: " + GameManager.singleton.currentScore;
         bestScoreText.text ="Best: " + GameManager.singleton.bestScore;
+
+        ChangeSlider();
+    }
+
+    public void ChangeSlider()
+    {
+        actualLevel.text = "" + (GameManager.singleton.currentLevel+1);
+        nextLevel.text = "" + (GameManager.singleton.currentLevel+2);
+
+        float totalDistance = topTrensform.position.y - goalTrensform.position.y;
+
+        float distanceLeft = totalDistance - (ball.position.y - goalTrensform.position.y);
+
+        float value = (distanceLeft / totalDistance);
+
+        slider.value = Mathf.Lerp(slider.value, value, 5);
     }
 
 
